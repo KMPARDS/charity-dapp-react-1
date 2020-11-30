e28d88547e357a0a0696348d655af61f93347481
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2020-12-01 03:17:27 +0530

    Add smart contract

--- src/ethereum/instances.ts
+++ src/ethereum/instances.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers';
 import { CustomProvider,  addresses } from 'eraswap-sdk';
+import { CharityDappCharityDappFactory } from './CharityDappCharityDappFactory';
 // import { CustomProvider } from './custom-providers';
 
 // import {
@@ -9,8 +10,11 @@ const config = addresses[process.env.REACT_APP_ENV === 'production' ? 'productio
 //@ts-ignore
 // window.providerESN = new CustomProvider('mainnet');
 window.provider = new ethers.providers.JsonRpcProvider('https://mainnet.eraswap.network');
-
+window.charityInstance = CharityDappCharityDappFactory.connect(
+  '0x3c5C0a207bdED8475AC192723a3db1BB44cE6768',
+  window.provider
+)
 // Temporary wallet
-if (process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY) {
-  window.wallet = new ethers.Wallet(process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY, window.provider);
-} 
+// if (process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY) {
+//   window.wallet = new ethers.Wallet(process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY, window.provider);
+// } 
--- src/global.ts
+++ src/global.ts
@@ -2,6 +2,7 @@ import { ethers } from 'ethers';
 
 // import { SurveyBuildSurvey } from './ethereum/typechain/SurveyBuildSurvey';
 import { CustomProvider } from 'eraswap-sdk';
+import { CharityDappCharityDapp } from './ethereum/CharityDappCharityDapp';
 
 // mark the typings of your global variables
 declare global {
@@ -10,6 +11,7 @@ declare global {
     providerESN: CustomProvider;
     wallet: any;
     ethereum: ethers.providers.ExternalProvider;
+    charityInstance : CharityDappCharityDapp;
      
     // wallet: CustomWallet | ethers.providers.JsonRpcSigner | undefined; // marking this as undefined helps to prevent many runtime bugs when wallet is not loaded
   }
